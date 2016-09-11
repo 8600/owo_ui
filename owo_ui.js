f3139129fd2b15a5e8ed41b9ee63186446c1cefc
Pandelion <100284685@qq.com>
2016-09-11 21:39:20 +0800

    增加图片处理

--- owo_ui.js
+++ owo_ui.js
@@ -26,45 +26,24 @@
         }
         else console.error("owo_ui [sizeTransition:"+"参数1 不是一个可操作的DOM对象并且取不到id为 参数1 的元素!]");
     }
-	//                     
-    //－－－－－－－－－－－－－－－－－－－－－－
-    // [参数]           target         grayscale
-    // [含义]           目标元素         灰度
-    // [重要性]           必填           必填
-    // [默认值]            无            100%
-    // [类型 or 单位]  字符串 or DOM    1%-100%
-    //－－－－－－－－－－－－－－－－－－－－－－
-	function grayscale(target,grayscale){
-        grayscale=changeResult||"100%";
-        if(typeof target === "string") target=document.getElementById(target);
-		target.style.cssText="filter: grayscale("+grayscale+");-webkit-filter: grayscale("+grayscale+");-moz-filter: grayscale("+grayscale+");-o-filter: grayscale("+grayscale+");-ms-filter: grayscale("+grayscale+");";
+	//                     给图片加上特效     
+    //－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+    // [参数]           target         attribute    value
+    // [含义]           目标元素         特效        值
+    // [重要性]           必填           必填       必填
+    // [默认值]            无            无          无
+    // [类型 or 单位]  字符串 or DOM    字符串      字符串
+    //－－－－－－－－－－－－－－－－－－－－－－－－－－－－
+	
+	//－－－－－－－－－－－attribute－－－－－－－－－－－－
+	//  [种类]      grayscale        blur      saturate       sepia       hue-rotate    invert   brightness  contrast
+	//  [含义]        灰度           模糊       饱和度        老化         色相旋转    底片效果    高亮        均衡
+	// [接受值]  0-1 or 1%-100%    0px-100px   0%-1000%   0-1 or 1%-100%  0deg-360deg    0-1      0%-1000%      0-1
+	function grayscale(target,attribute,value){
+        if(typeof target === "string") {target=document.getElementById(target);}
+		if(target.tagName!=="IMG"){console.log("你输入的元素不是图片哦!");return;}
+		target.style.cssText="filter: "+attribute+"("+value+");-webkit-filter: "+attribute+"("+value+");-moz-filter: "+attribute+"("+value+");-o-filter: "+attribute+"("+value+");-ms-filter: "+attribute+"("+value+");";
 	}
 	
-		//                     
-    //－－－－－－－－－－－－－－－－－－－－－－
-    // [参数]           target         grayscale
-    // [含义]           目标元素         灰度
-    // [重要性]           必填           必填
-    // [默认值]            无            100%
-    // [类型 or 单位]  字符串 or DOM    1%-100%
-    //－－－－－－－－－－－－－－－－－－－－－－
-	function grayscale(target,grayscale){
-        grayscale=changeResult||"100%";
-        if(typeof target === "string") target=document.getElementById(target);
-		target.style.cssText="filter: grayscale("+grayscale+");-webkit-filter: grayscale("+grayscale+");-moz-filter: grayscale("+grayscale+");-o-filter: grayscale("+grayscale+");-ms-filter: grayscale("+grayscale+");";
-	}
-	
-		//                     
-    //－－－－－－－－－－－－－－－－－－－－－－
-    // [参数]           target         grayscale
-    // [含义]           目标元素        模糊值
-    // [重要性]           必填           必填
-    // [默认值]            无            5px
-    // [类型 or 单位]  字符串 or DOM     像素
-    //－－－－－－－－－－－－－－－－－－－－－－
-	function blur(target,grayscale){
-        grayscale=changeResult||"5px";
-        if(typeof target === "string") target=document.getElementById(target);
-		target.style.cssText="filter: blur("+grayscale+");-webkit-filter: blur("+grayscale+");-moz-filter: blur("+grayscale+");-o-filter: blur("+grayscale+");-ms-filter: blur("+grayscale+");";
-	}
+
 	
